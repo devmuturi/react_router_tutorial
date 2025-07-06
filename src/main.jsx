b636ac12cef4297e726af16ca8d5775f4be09bb4
devmuturi <muturinjonge09@gmail.com>
2025-07-06 15:26:16 +0300

    Create the root route

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Hello world!</h1>
+    element: <Root />,
   }
 ])
 
